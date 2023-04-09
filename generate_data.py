aa1ba3532874b5af5f83fabe28b07207e5025e5c
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-04-09 22:05:39 +0300

    Added generation functions.

--- generate_data.py
+++ generate_data.py
@@ -29,14 +29,51 @@ def createPlayers():
         sqls.append(sql_insert_modified)
 
 
-    f = open('final_data.sql', 'w')
+    f = open('data_player.sql', 'w')
     # write SQL statements to file in /tmp directory
     for i, sql in enumerate(sqls):
         f.write(sql + "\n")
     f.close()
 
+def createWeapons():
+    fake = Faker()
+    sqls = []
+    allClasses = ['Hunter', 'Titan', 'Warlock']
+
+    # generate fake data for players and create INSERT SQL statements
+    for j in range(1000):
+        sql_insert = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES"
+        for i in range(1000):
+            name = fake.name()
+
+            random_class_id = random.randint(0, 2)
+            class1 = allClasses[random_class_id]
+
+            level = random.randint(1, 50)
+            glimmer = random.randint(0, 250000)
+            shards = random.randint(0, 1000000)
+            #sql = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES ('{}', '{}', {}, {}, {})".format(
+            #    name, class1, level, glimmer, shards)
+            sql_insert = sql_insert + " ('{}', '{}', {}, {}, {}), ".format(name, class1, level, glimmer, shards)
+        insert_size = len(sql_insert)
+        sql_insert_modified = sql_insert[:insert_size - 2]
+        sql_insert_modified = sql_insert_modified + ";"
+
+        sqls.append(sql_insert_modified)
+
+
+    f = open('data_player.sql', 'w')
+    # write SQL statements to file in /tmp directory
+    for i, sql in enumerate(sqls):
+        f.write(sql + "\n")
+    f.close()
+
+
 def main():
     createPlayers()
+    #createWeapons()
+    #createLocations()
+    #createLocationWeapons()
 
 if __name__ == '__main__':
     main()
