901db993934837520b120ca93c8b351089f7b131
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-04-09 22:25:32 +0300

    Added generation functions.

--- generate_data.py
+++ generate_data.py
@@ -8,7 +8,7 @@ def createPlayers():
     allClasses = ['Hunter', 'Titan', 'Warlock']
 
     # generate fake data for players and create INSERT SQL statements
-    for j in range(1000):
+    for j in range(1):
         sql_insert = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES"
         for i in range(1000):
             name = fake.name()
@@ -34,27 +34,36 @@ def createPlayers():
     for i, sql in enumerate(sqls):
         f.write(sql + "\n")
     f.close()
+
 
 def createWeapons():
     fake = Faker()
     sqls = []
-    allClasses = ['Hunter', 'Titan', 'Warlock']
+    allElements = ['Kinetic', 'Solar', 'Arc', 'Void', 'Strand', 'Strand']
+    allTypes = ['Hand cannon', 'Auto rifles', 'Shotgun', 'Fusion Rifle', 'Linear Fusion Rifle', 'Bow', 'Sniper Rifle',
+                'Scout Rifle', 'Submachine gun', 'Pulse Rifle', 'Rocket Launcher']
+    allSlots = ['Kinetic', 'Energy', 'Heavy']
+
 
     # generate fake data for players and create INSERT SQL statements
-    for j in range(1000):
-        sql_insert = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES"
+    for j in range(1):
+        sql_insert = "INSERT INTO destinycharacters_weapon (weapon_name, weapon_slot, weapon_element, weapon_type, weapon_damage, player_weapon) VALUES"
         for i in range(1000):
-            name = fake.name()
+            weapon_name = fake.word()
 
-            random_class_id = random.randint(0, 2)
-            class1 = allClasses[random_class_id]
+            random_slot = random.randint(0, len(allSlots)-1)
+            random_element = random.randint(0, len(allElements)-1)
+            random_type = random.randint(0, len(allTypes)-1)
+            weapon_damage = random.randint(0, 450)
+            player_weapon = random.randint(1, 1000000)
 
-            level = random.randint(1, 50)
-            glimmer = random.randint(0, 250000)
-            shards = random.randint(0, 1000000)
+            weapon_slot = allSlots[random_slot]
+            weapon_element = allElements[random_element]
+            weapon_type = allTypes[random_type]
+
             #sql = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES ('{}', '{}', {}, {}, {})".format(
             #    name, class1, level, glimmer, shards)
-            sql_insert = sql_insert + " ('{}', '{}', {}, {}, {}), ".format(name, class1, level, glimmer, shards)
+            sql_insert = sql_insert + " ('{}', '{}', '{}', '{}', {}, {}), ".format(weapon_name, weapon_slot, weapon_element, weapon_type, weapon_damage, player_weapon)
         insert_size = len(sql_insert)
         sql_insert_modified = sql_insert[:insert_size - 2]
         sql_insert_modified = sql_insert_modified + ";"
@@ -62,18 +71,82 @@ def createWeapons():
         sqls.append(sql_insert_modified)
 
 
-    f = open('data_player.sql', 'w')
+    f = open('data_weapons.sql', 'w')
     # write SQL statements to file in /tmp directory
     for i, sql in enumerate(sqls):
         f.write(sql + "\n")
     f.close()
 
+def createLocations():
+    fake = Faker()
+    sqls = []
+    allEnemies = ['Fallen', 'Scorn', 'Cabal', 'Vex', 'Taken']
+
+
+    # generate fake data for players and create INSERT SQL statements
+    for j in range(1):
+        sql_insert = "INSERT INTO destinycharacters_location (location_name, enemy_type, min_level, nr_public_events, nr_lost_sectors) VALUES"
+        for i in range(1000):
+            location_name = fake.city()
+
+            random_enemy = random.randint(0, len(allEnemies)-1)
+            enemy_type = allEnemies[random_enemy]
+            min_level = random.randint(1, 50)
+            nr_public_events = random.randint(0, 10)
+            nr_lost_sectors = random.randint(3, 7)
+
+
+            #sql = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES ('{}', '{}', {}, {}, {})".format(
+            #    name, class1, level, glimmer, shards)
+            sql_insert = sql_insert + " ('{}', '{}', {}, {}, {}), ".format(location_name, enemy_type, min_level, nr_public_events, nr_lost_sectors)
+        insert_size = len(sql_insert)
+        sql_insert_modified = sql_insert[:insert_size - 2]
+        sql_insert_modified = sql_insert_modified + ";"
+
+        sqls.append(sql_insert_modified)
+
+
+    f = open('data_locations.sql', 'w')
+    # write SQL statements to file in /tmp directory
+    for i, sql in enumerate(sqls):
+        f.write(sql + "\n")
+    f.close()
+
+def createLocationWeapons():
+
+    sqls = []
+
+    # generate fake data for players and create INSERT SQL statements
+    for j in range(1):
+        sql_insert = "INSERT INTO destinycharacters_location_weapon (drop_rate, wep, loc) VALUES"
+        for i in range(1000):
+
+            drop_rate = random.randint(1, 100)
+            wep = random.randint(1, 1000000)
+            loc = random.randint(1, 1000000)
+
+
+            #sql = "INSERT INTO destinycharacters_player (name, class1, level, glimmer, shards) VALUES ('{}', '{}', {}, {}, {})".format(
+            #    name, class1, level, glimmer, shards)
+            sql_insert = sql_insert + " ({}, {}, {}), ".format(drop_rate, wep, loc)
+        insert_size = len(sql_insert)
+        sql_insert_modified = sql_insert[:insert_size - 2]
+        sql_insert_modified = sql_insert_modified + ";"
+
+        sqls.append(sql_insert_modified)
+
+
+    f = open('data_location_weapons.sql', 'w')
+    # write SQL statements to file in /tmp directory
+    for i, sql in enumerate(sqls):
+        f.write(sql + "\n")
+    f.close()
 
 def main():
     createPlayers()
-    #createWeapons()
-    #createLocations()
-    #createLocationWeapons()
+    createWeapons()
+    createLocations()
+    createLocationWeapons()
 
 if __name__ == '__main__':
     main()
