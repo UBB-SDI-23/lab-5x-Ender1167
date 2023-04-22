bb2234e82e229ac636c2b19ebe9e2daf40cc7db7
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-04-22 12:38:30 +0300

    Pagination attempt 1.0.

--- destinycharacters/settings.py
+++ destinycharacters/settings.py
@@ -92,7 +92,10 @@ DATABASES = {
    }
 }
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+}
 
 
 # Password validation
