a0fd7c46a23725b5a8a79a2e66782ffb4fa88688
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-07 00:38:15 +0300

    Weapon edit and update frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import Modal from "./components/Modal";
-import Weapon from "./components/Weapon";
 import React, { Component } from "react";
 import axios from "axios";
 import {
