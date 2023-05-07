8484b141ebe432a9483aa9bcc3a9e8372c4b14af
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-07 22:12:39 +0300

    Frontend fix.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -470,7 +470,7 @@ class App extends Component {
           }`}
           title={item.name}
         >
-          {item.weapon_name + "/" + item.nr_loc}
+          {item.weapon_name}
         </span>
 		<span>
           <button
