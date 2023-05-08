648788b9201bd6c66d25759ebe592e4a2ff23308
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-08 08:25:20 +0300

    Validation fix backend

--- destinycharacters/models.py
+++ destinycharacters/models.py
@@ -30,7 +30,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True, null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
-    age = models.IntegerField(validators=[validateAge])
+    age = models.IntegerField(validators=[validateAge], null=True)
     gender = models.CharField(max_length=30, choices=GENDER_CHOICES, default=male, blank=True, null=True)
     marital_status = models.CharField(max_length=30, choices=MARITAL_CHOICES, default=not_married, blank=True, null=True)
 
