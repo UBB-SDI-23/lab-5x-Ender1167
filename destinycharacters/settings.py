8b2b9352f22a916881078aa070227da01ee5603a
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-11 19:01:20 +0300

    Login attempt 5.

--- destinycharacters/settings.py
+++ destinycharacters/settings.py
@@ -109,7 +109,7 @@ REST_FRAMEWORK = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-#AUTH_USER_MODEL = "destinycharacters.UserProfile"
+AUTH_USER_MODEL = "destinycharacters.UserProfile"
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
