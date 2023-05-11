9941fb9cb553f160cd5d0f1722045db5b5c0ad57
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-11 19:04:32 +0300

    Login attempt 5.

--- destinycharacters/models.py
+++ destinycharacters/models.py
@@ -148,45 +148,5 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_superuser', True)
         return self.create_user(username, password, **extra_fields)
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password, **extra_fields):
-        """
-        Creates and saves a User with the given username and password.
-        """
-        if not username:
-            raise ValueError('The Username field must be set')
 
-        user = self.model(username=username, **extra_fields)
-        user.set_password(password)
-        print(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, password=None, **extra_fields):
-        """
-        Creates and saves a superuser with the given username and password.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, password, **extra_fields)
-
-class UserProfile(AbstractUser):
-    not_married = "Not married"
-    MALE = "Male"
-    FEMALE = "Female"
-    MARITAL_CHOICES = [
-        ("Married", "Married"),
-        ("Not married", "Not married")
-    ]
-    GENDER_CHOICES = [
-        (MALE, "Male"),
-        (FEMALE, "Female")
-    ]
-
-    #user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True, null=True)
-    location = models.CharField(max_length=30, blank=True, null=True)
-    age = models.IntegerField(validators=[validateAge], null=True)
-    gender = models.CharField(max_length=30, choices=GENDER_CHOICES, default=MALE, blank=True, null=True)
-    marital_status = models.CharField(max_length=30, choices=MARITAL_CHOICES, default=not_married, blank=True, null=True)
 
