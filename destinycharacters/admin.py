936b8dedf22262a306c317d11169023ed2e6b27e
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-11 18:45:02 +0300

    Login attempt 5.

--- destinycharacters/admin.py
+++ destinycharacters/admin.py
@@ -3,10 +3,10 @@ from .models import Player
 from .models import Weapon
 from .models import Location
 from .models import Location_Weapon
-#from .models import UserProfile
+from .models import UserProfile
 
 admin.site.register(Player)
 admin.site.register(Weapon)
 admin.site.register(Location)
 admin.site.register(Location_Weapon)
-#admin.site.register(UserProfile)
+admin.site.register(UserProfile)
