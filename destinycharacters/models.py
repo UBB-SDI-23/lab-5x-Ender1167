f5b0a21dc661e90a777c464f1ad24303e829c019
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-12 16:25:39 +0300

    Backend profile creation 3.

--- destinycharacters/models.py
+++ destinycharacters/models.py
@@ -176,10 +176,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        userpf = UserProfile.objects.create(user=instance)
-        serializer = UserSerializer(userpf)
-        userpf.isActive = False
-        serializer.save()
+        UserProfile.objects.create(user=instance)
 
 #this method to update profile when user is updated
 @receiver(post_save, sender=User)
