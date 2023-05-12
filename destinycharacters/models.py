bd7c0c72e62e5c530f02421d56af1a96e2e3f2e3
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-12 15:07:14 +0300

    Backend registration

--- destinycharacters/models.py
+++ destinycharacters/models.py
@@ -173,7 +173,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance, isActive=False)
+        UserProfile.objects.create(user=instance)
 
 #this method to update profile when user is updated
 @receiver(post_save, sender=User)
