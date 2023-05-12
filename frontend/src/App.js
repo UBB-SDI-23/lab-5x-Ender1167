b0fb5b23c675d2eba3adca8d30187a826eb5472b
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-12 13:25:41 +0300

    Frontend profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -278,8 +278,6 @@ class App extends Component {
         </div>
     )
 }  
-	  
-  };
   handleRegister = async(item) => {
 
     this.toggleRegister();
@@ -741,7 +739,7 @@ class App extends Component {
     return (
       <main className="container">
         <h1 className="text-black text-uppercase text-center my-4">Destiny Characters</h1>
-
+		{this.state.isAuth ? this.getProfile() : null}
 		<p id="error1" className="text-center"></p>
         <div className="row">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
