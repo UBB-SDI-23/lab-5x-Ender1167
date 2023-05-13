e1e001d1ac812dcf2009544fb617124ad21222be
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-13 14:15:50 +0300

    Backend registration with token 5.

--- destinycharacters/views.py
+++ destinycharacters/views.py
@@ -361,7 +361,7 @@ class RegisterFromToken(APIView):
 
     def post(self, request, format=None):
         token1 = self.serializer_class(data=request.data)
-        token_user_username = token1.initial_data['token']['username']
+        token_user_username = token1.initial_data['token'][0]
         token_user_password = token1.initial_data['token']
         return Response({
             "message": token_user_username,
