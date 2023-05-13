1317a8ceea3ac46be6545ef51bf97366297eaf71
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-13 17:36:33 +0300

    Frontend activation 4.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -335,13 +335,14 @@ class App extends Component {
     await axios
       .post("/api/register/", item)
 	  .then((res) => this.setState({authToken: res.data.access}, () => {
-		  document.getElementById("error1").innerHTML = this.state.authToken;
+		    document.getElementById("error1").innerHTML = this.state.authToken;
+		  	const newToken = { token: this.state.authToken };
+	        this.setState({activeToken: newToken});
+	        this.setState({modal_type : 6});
+	        this.toggleToken();
 	  }));
 	  
-	const newToken = { token: this.state.authToken };
-	this.setState({activeToken: newToken});
-	this.setState({modal_type : 6});
-	this.toggleToken();
+
   };
   
    handleActivation = async(item) => {
