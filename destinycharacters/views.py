f0a981ab8e29b9ab16f09d67ad8d24e1e140bf46
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-13 14:34:54 +0300

    Backend registration with token 11.

--- destinycharacters/views.py
+++ destinycharacters/views.py
@@ -370,7 +370,9 @@ class RegisterFromToken(APIView):
                                   algorithms=["HS256"])
         token_user_username = decoded_data['username']
         token_user_password = decoded_data['password']
-
+        return Response({
+            "message": "Activation for" + token_user_username + " with password " + token_user_password + " is successful",
+        })
         if token_user_username != None:
             user = authenticate(username=token_user_username, password=token_user_password)
             if user.profile.isActive == True:
