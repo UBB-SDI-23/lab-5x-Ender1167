4cc5ee5b90c4164f1c24c991c6652a4093cedd50
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-13 18:01:11 +0300

    Backend and frontend activation.

--- destinycharacters/models.py
+++ destinycharacters/models.py
@@ -173,7 +173,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        u = UserProfile.objects.create(user=instance)
+        u = UserProfile.objects.create_user(user=instance)
         u.user.is_active = False
         u.isActive = False
         instance.save()
