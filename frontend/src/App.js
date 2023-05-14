c5cc93e06f6b0652ab32f01437a3cbcda82e428e
Ender1167 <dragos.duca@stud.ubbcluj.ro>
2023-05-14 21:18:08 +0300

    Frontend changes 6.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -882,7 +882,7 @@ class App extends Component {
       <main className="container">
         <h1 className="text-black text-uppercase text-center my-4">Destiny Characters</h1>
 		
-		<div className="card p-3">
+		<div class="card">
 		<p id="error1" className="text-right">Anonymous user</p>
 		</div>
         <div className="row">
